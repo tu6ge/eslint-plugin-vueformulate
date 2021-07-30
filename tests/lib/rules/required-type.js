0e6a34c3c82059536da6ce60dbe913da6226c329
tu6ge <772364230@qq.com>
2021-07-30 16:45:33 +0800

    feat: add required-type rule

--- tests/lib/rules/required-type.js
+++ tests/lib/rules/required-type.js
@@ -6,14 +6,69 @@ var ruleTester = new RuleTester({
   parserOptions: { ecmaVersion: 2015 }
 })
 ruleTester.run("required-type", rule, {
-  valid: [{
-    filename: 'test.vue',
-    code: '<template><FormulateInput type="demo"></FormulateInput></template>',
-    options: ['always']
-  }],
-  invalid: [{
-    filename: 'test.vue',
-    code: '<template><FormulateInput></FormulateInput></template>',
-    options: ['always']
-  }]
+  valid: [
+    {
+      filename: 'test.vue',
+      code: '<template><FormulateInput type="demo"></FormulateInput></template>',
+      options: ['always']
+    },
+    {
+      filename: 'test.vue',
+      code: '<template><formulate-input type="demo"></formulate-input></template>',
+      options: ['always']
+    },
+    {
+      filename: 'test.vue',
+      code: '<template><FormulateInput type="demo" validation=""></FormulateInput></template>',
+      options: ['always']
+    },
+    {
+      filename: 'test.vue',
+      code: '<template><FormulateInput validation="" type="demo"></FormulateInput></template>',
+      options: ['always']
+    },
+    {
+      filename: 'test.vue',
+      code: '<template><div><FormulateInput type="demo"></FormulateInput></div></template>',
+      options: ['always']
+    },
+  ],
+  invalid: [
+    {
+      filename: 'test.vue',
+      code: '<template><FormulateInput></FormulateInput></template>',
+      options: ['always'],
+      errors: [
+        {
+          message: "formulate-input need have type prop",
+          type: 'VElement',
+          line: 1
+        }
+      ]
+    },
+    {
+      filename: 'test.vue',
+      code: '<template><FormulateInput validation=""></FormulateInput></template>',
+      options: ['always'],
+      errors: [
+        {
+          message: "formulate-input need have type prop",
+          type: 'VElement',
+          line: 1
+        }
+      ]
+    },
+    {
+      filename: 'test.vue',
+      code: '<template><div><FormulateInput validation=""></FormulateInput></div></template>',
+      options: ['always'],
+      errors: [
+        {
+          message: "formulate-input need have type prop",
+          type: 'VElement',
+          line: 1
+        }
+      ]
+    }
+  ]
 });
